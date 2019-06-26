71375eec6a7adaeddeaad3a1850b5fc5eabe023e
Matheus Rossi <matheus.rossi@libre.local>
2019-06-26 15:15:20 -0300

    First Commit

---
+++ src/layouts/MyLayout.vue
@@ -0,0 +1,50 @@
+
+<template>
+  <q-layout view="hHh lpR lFr">
+
+    <q-header class="bg-primary text-white">
+      <q-toolbar>
+        <q-btn dense flat round icon="menu" @click="left = !left" />
+
+        <q-toolbar-title>
+          <q-avatar>
+
+          </q-avatar>
+          Lean App
+        </q-toolbar-title>
+
+        <q-btn dense flat round icon="menu" @click="right = !right" />
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer v-model="left" side="left" overlay behavior="mobile">
+      <!-- drawer content -->
+    </q-drawer>
+
+    <q-drawer v-model="right" side="right" overlay behavior="mobile">
+      <!-- drawer content -->
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+  </q-layout>
+</template>
+
+<script>
+import { openURL } from 'quasar'
+
+export default {
+  name: 'MyLayout',
+  data () {
+    return {
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
+<style>
+</style>
