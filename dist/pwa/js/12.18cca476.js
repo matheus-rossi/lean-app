f62990befb1443efa6c475821cf81a219f450cff
Matheus Rossi <matheus.rossi@libre.local>
2020-09-07 13:03:34 -0300

    lean-app refactored to new quasar version and yarn

---
+++ dist/pwa/js/12.18cca476.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([[12],{ece3:function(e,n,t){"use strict";t.r(n);var c=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div")},l=[],u={},r=u,s=t("2877"),a=Object(s["a"])(r,c,l,!1,null,null,null);n["default"]=a.exports}}]);
